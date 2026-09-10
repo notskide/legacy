401957019bd500d283f5284f968116f7fdcc2767
notskide <Skideprob@gmail.com>
2026-09-10 15:16:22 +0300

    Legacy V 1.1.5

--- app.py
+++ app.py
@@ -170,4 +170,4 @@ def api_search():
     return Response(response_text, mimetype="text/plain")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
